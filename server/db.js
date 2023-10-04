c3cfe3b67b556c95946a98c7a6a83434d94887fb
ajlittlebro <nanain.mozo3@gmail.com>
2023-10-04 00:32:04 -0600

    extension .js to config in db.js added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
 
 export const pool = new createPool({
   host: DB_HOST,
