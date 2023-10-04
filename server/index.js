2e72d4f35cc20928094a166afa11f6ce37778465
ajlittlebro <nanain.mozo3@gmail.com>
2023-10-03 23:04:36 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { PORT } from "./config.js";
+import indexRoutes from "./routes/index.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
+import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+
+app.use(cors());
+app.use(express.json());
+app.use(taskRoutes);
+app.use(indexRoutes);
+app.use(express.static(join(__dirname, "../client/dist")));
+
+app.listen(PORT);
+console.log("Server is listening on port " + PORT);
